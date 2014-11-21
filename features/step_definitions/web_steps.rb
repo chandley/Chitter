856d51139061f1aa384da7509663215b1951ba0d
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-11-21 12:30:51 +0000

    setup app with cucumber tests. Some configuration issues

---
+++ features/step_definitions/web_steps.rb
@@ -0,0 +1,25 @@
+require 'uri'
+require 'cgi'
+require './server.rb'
+# require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+
+
+Given(/^an anonymous visitor$/) do
+  @session = Server.new
+end
+
+When(/^I visit the homepage$/) do
+  visit('/')
+end
+
+Then(/^I should see a welcome message$/) do
+  expect(page).to have_content('Welcome to Chitter')
+end
+
+Given(/^a message has been posted$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see at least one message$/) do
+  pending # express the regexp above with the code you wish you had
+end
